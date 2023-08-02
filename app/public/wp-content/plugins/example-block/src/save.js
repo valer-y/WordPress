c89736b0acf3ba656c86af1470f027ff7075bd75
Valery_Yazykov <yazykov.valery@gmail.com>
2023-08-02 18:48:15 +0300

    feature-3: add color editor to block background and text

--- app/public/wp-content/plugins/example-block/src/save.js
+++ app/public/wp-content/plugins/example-block/src/save.js
@@ -17,11 +17,13 @@ import { useBlockProps, RichText } from '@wordpress/block-editor';
  */
 export default function save( { attributes } ) {
 	const blockProps = useBlockProps.save();
+	const { content, align, backgroundColor, textColor } = attributes;
 	return (
 		<RichText.Content
 			{ ...blockProps }
 			tagName="p"
-			value={ attributes.content }
+			value={ content }
+			style={ { textAlign: align, backgroundColor: backgroundColor, color: textColor } }
 		/>
 	);
 }
