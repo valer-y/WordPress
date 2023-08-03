c77a26b47b19be6ef0ca5dfd6c8cc84404b7b7a8
Valery_Yazykov <yazykov.valery@gmail.com>
2023-08-03 17:26:34 +0300

    feature-1: firstblock latout

--- app/public/wp-content/plugins/firstblock/firstblock.php
+++ app/public/wp-content/plugins/firstblock/firstblock.php
@@ -1,11 +1,15 @@
 <?php
-/**
-* Plugin name: First block
-* Description: First test block
-*/
 
-function test_block_init() {
-	register_block_type_from_metadata(__DIR__);
+/**
+ * Plugin Name: First Block
+ * Plugin URI: https://alialaa.com/
+ * Description: My first block
+ * Author: Ali Alaa
+ * Author URI: https://alialaa.com/
+ */
+
+function firstblock_init() {
+    register_block_type_from_metadata( __DIR__ );
 }
 
-add_action("init", "test_block_init");
+add_action( "init", "firstblock_init" );
