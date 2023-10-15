71a8425cce40b4c09dfedb38348486b5f31c457a
Valery Yazykov <valery.yazykov@interexy.com>
2023-10-15 13:52:02 +0300

    feature-1.2: add image

--- app/public/wp-content/plugins/gutenberg-boilerplate-cta/src/edit.js
+++ app/public/wp-content/plugins/gutenberg-boilerplate-cta/src/edit.js
@@ -1,11 +1,42 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText, MediaPlaceholder, BlockControls, MediaReplaceFlow } from '@wordpress/block-editor';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({attributes, setAttributes}) {
+	const {description, id, url, alt} = attributes;
+	const onChangeDescritption = (newDescription) => {
+		setAttributes({description: newDescription})
+	}
+	const onSelectImage = (image) => {
+		if(!image || !image.url) {
+			setAttributes( {url: undefined, id: undefined, alt: ''} );
+			return;
+		}
+		setAttributes( { url: image.url, id: image.id, alt: image.alt } );
+	}
+
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<BlockControls>
+			<section {...useBlockProps()}>
+				{ url && <img src={ url } alt={ alt } /> }
+				<MediaPlaceholder icon={"smiley"}
+								  onSelect = { onSelectImage }
+								  onError = { (e) => console.log(e) }
+								  accept="image/*"
+								  allowedTypes={['image']}
+								  disableMediaButtons = {url}
+
+				/>
+				<RichText placeholder={__('Placeholder text', 'cta')}
+						  tagname="p"
+						  onChange={onChangeDescritption}
+						  vaue = {description}
+						  allowedFormats = {[
+							  'core/bold',
+							  'core/link'
+						  ]}
+				/>
+			</section>
+		</BlockControls>
 	);
 }
