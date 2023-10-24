c95463127d8188fb2cb4dc38e47badb460351ed2
Valery Yazykov <valery.yazykov@interexy.com>
2023-10-24 13:15:51 +0300

    improve gallery for 6 img admin styles

--- app/public/wp-content/mu-plugins/gutenberg-multi-gallery/build/index.asset.php
+++ app/public/wp-content/mu-plugins/gutenberg-multi-gallery/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'dc59edbf899dd0ece936');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2f8765f8b0bc75f440df');
