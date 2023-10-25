6ba23cf697ef223339f0e8688cec7ab688d08c75
Valery Yazykov <valery.yazykov@interexy.com>
2023-10-25 16:19:55 +0300

    feature-6: text cards

---
+++ app/public/wp-content/mu-plugins/text-cards-gallery/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '7e41315c1ef889ca2754');
---
+++ app/public/wp-content/mu-plugins/text-cards-gallery/src/card/edit.js
@@ -0,0 +1,55 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit({attributes, setAttributes}) {
+	const { heading, text } = attributes;
+
+	return (
+		<>
+			<div {...useBlockProps.save({
+				className: "block-text-card"
+			})}>
+				<RichText {...useBlockProps()}
+						  placeholder={'Placeholder heading'}
+						  onChange={ (h) => {setAttributes({heading: h})}}
+						  value={ heading }
+						  tagName="h4"
+				/>
+
+				<RichText {...useBlockProps()}
+						  placeholder={'Placeholder text'}
+						  onChange={ (u) => {setAttributes({text: u})}}
+						  value={ text }
+						  tagName="p"
+				/>
+			</div>
+		</>
+	);
+}
